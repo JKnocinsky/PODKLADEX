807cde2988f3a632fc781ca1ab42b0b16e06cef0
79180FP <79180FP@SWML-LTC27>
2026-03-18 15:01:14 +0100

    Dodanie formularza Kadry i finanse

---
+++ PodkladexApp/Form_Kadry_finanse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PodkladexApp
+{
+    public partial class Form_Kadry_finanse : Form
+    {
+        public Form_Kadry_finanse()
+        {
+            InitializeComponent();
+        }
+    }
+}
