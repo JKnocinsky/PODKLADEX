881ac0d26f0e313d1e2a1517338a93000afac2f2
79312SW <79312SW@SWML-LTC24>
2026-03-18 16:15:25 +0100

    zatwierdzenie

--- PodkladexApp/Form_Menu.cs
+++ PodkladexApp/Form_Menu.cs
@@ -3,12 +3,15 @@ namespace PodkladexApp
     public partial class Form_Menu : Form
     {
         Pokemon pokemon1;
-
+        Pokemon pokemon5;
         public Form_Menu()
         {
             InitializeComponent();
             pokemon1 = new Pokemon();
+            pokemon5 = new Pokemon("Charmander", "Ogieñ");
 
+            pokemon5.WydajDzwiek("Aaaaaaaaaa!");
+            pokemon5.Atak_pokemona("P³omieñ");
             pokemon1.WydajDzwiek("Pika Pika!");
             pokemon1.Atak_pokemona("Piorun");
 
