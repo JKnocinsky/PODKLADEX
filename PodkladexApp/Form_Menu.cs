16d13f7e4728eeb62b047beb5834700ef8f19a26
76587MW <76587MW@SWML-LTC28>
2026-03-18 16:16:34 +0100

    Poszerzono klasę Pokemon o niezbędne elementy

--- PodkladexApp/Form_Menu.cs
+++ PodkladexApp/Form_Menu.cs
@@ -3,6 +3,11 @@ namespace PodkladexApp
     public partial class Form_Menu : Form
     {
         Pokemon pokemon1;
+        Pokemon pokemon2;
+        Pokemon pokemon3;
+        Pokemon pokemon4;
+        Pokemon pokemon5;
+        Pokemon pokemon6;
 
         public Form_Menu()
         {
@@ -11,6 +16,11 @@ namespace PodkladexApp
 
             pokemon1.WydajDzwiek("Pika Pika!");
             pokemon1.Atak_pokemona("Piorun");
+
+            pokemon3 = new Pokemon("Bulbasaur", "Trawiasty", "Heteroseksualny", "Ateista", "Centrolewicowy");
+            pokemon4 = new Pokemon("Magikarp", "Wodny", "Biseksualny", "Ateista", "Centroprawicowy");
+            pokemon5 = new Pokemon("Charizard", "Ognisty/Lataj¹cy", "Heteroseksualny", "Ateista", "Centroprawicowy");
+            pokemon6 = new Pokemon( "Squirtle", "Wodny", "Heteroseksualny", "Ateista", "Centrolewicowy");
 
         }
     }
--- PodkladexApp/Pokemon.cs
+++ PodkladexApp/Pokemon.cs
@@ -11,16 +11,24 @@ namespace PodkladexApp
         public string Nazwa {  get; set; }
         public string Rodzaj { get; set; }
         public int Numer {  get; set; }
+        public string Orientacja_seksulana { get; set; }
+
+        public string Wyznanie_religijne { get; set; }
+
+        public string Poglądy_polityczne { get; set; }
 
         public Pokemon()
         {
             Nazwa = "Pikachu";
             Rodzaj = "Elektryczny";
         }
-        public Pokemon(string Nazwa, string Rodzaj)
+        public Pokemon(string Nazwa, string Rodzaj, string Orientacja, string wyznanie_religijne, string poglądy_polityczne)
         {
             this.Nazwa = Nazwa;
             this.Rodzaj = Rodzaj;
+            this.Orientacja_seksulana = Orientacja;
+            this.Wyznanie_religijne = wyznanie_religijne;
+            this.Poglądy_polityczne = poglądy_polityczne;
         }
 
         public void Doswiadczenie()
