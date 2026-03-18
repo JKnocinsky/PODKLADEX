a1671afae75329f4190190b1f2da2aad367bae88
79180FP <79180FP@SWML-LTC27>
2026-03-18 16:09:38 +0100

    Dodanie Blastoise

--- PodkladexApp/Form_Menu.cs
+++ PodkladexApp/Form_Menu.cs
@@ -3,14 +3,20 @@ namespace PodkladexApp
     public partial class Form_Menu : Form
     {
         Pokemon pokemon1;
-
+        Pokemon pokemon2;
         public Form_Menu()
         {
             InitializeComponent();
             pokemon1 = new Pokemon();
+            pokemon2 = new Pokemon("Blastoise","Wodny");
 
             pokemon1.WydajDzwiek("Pika Pika!");
+
+            pokemon2.WydajDzwiek("Plum Plum");
+
             pokemon1.Atak_pokemona("Piorun");
+
+            pokemon2.Atak_pokemona("Mega Wyrzutnia");
 
         }
     }
