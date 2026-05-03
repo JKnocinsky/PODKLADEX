826357b37e098cafd456717cd7dc18524eb80b22
Mateusz <mateusz.miazga@student.wat.edu.pl>
2026-05-03 10:18:41 +0200

    Kontrolki do SzczegolyHistorii

--- PodkladexApp/Zaopatrzenie/Form_HistoriaZamowienSzczegoly.Designer.cs
+++ PodkladexApp/Zaopatrzenie/Form_HistoriaZamowienSzczegoly.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox_Klient = new TextBox();
+            textBox_DataZlozenia = new TextBox();
+            textBox_DataZrealizowania = new TextBox();
+            dataGridView_Koszyk = new DataGridView();
+            panel_Firmy = new Panel();
+            textBox_NIP = new TextBox();
+            textBox_NazwaFirmy = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView_Koszyk).BeginInit();
+            panel_Firmy.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox_Klient
+            // 
+            textBox_Klient.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBox_Klient.Location = new Point(83, 82);
+            textBox_Klient.Name = "textBox_Klient";
+            textBox_Klient.Size = new Size(250, 33);
+            textBox_Klient.TabIndex = 0;
+            textBox_Klient.TextChanged += textBox_Klient_TextChanged;
+            // 
+            // textBox_DataZlozenia
+            // 
+            textBox_DataZlozenia.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBox_DataZlozenia.Location = new Point(83, 172);
+            textBox_DataZlozenia.Name = "textBox_DataZlozenia";
+            textBox_DataZlozenia.Size = new Size(250, 33);
+            textBox_DataZlozenia.TabIndex = 1;
+            textBox_DataZlozenia.TextChanged += textBox_DataZlozenia_TextChanged;
+            // 
+            // textBox_DataZrealizowania
+            // 
+            textBox_DataZrealizowania.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBox_DataZrealizowania.Location = new Point(83, 271);
+            textBox_DataZrealizowania.Name = "textBox_DataZrealizowania";
+            textBox_DataZrealizowania.Size = new Size(250, 33);
+            textBox_DataZrealizowania.TabIndex = 2;
+            textBox_DataZrealizowania.TextChanged += textBox_DataZrealizowania_TextChanged;
+            // 
+            // dataGridView_Koszyk
+            // 
+            dataGridView_Koszyk.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView_Koszyk.Location = new Point(456, 46);
+            dataGridView_Koszyk.Name = "dataGridView_Koszyk";
+            dataGridView_Koszyk.Size = new Size(702, 410);
+            dataGridView_Koszyk.TabIndex = 3;
+            dataGridView_Koszyk.CellContentClick += dataGridView_Koszyk_CellContentClick;
+            // 
+            // panel_Firmy
+            // 
+            panel_Firmy.Controls.Add(textBox_NIP);
+            panel_Firmy.Controls.Add(textBox_NazwaFirmy);
+            panel_Firmy.Location = new Point(12, 345);
+            panel_Firmy.Name = "panel_Firmy";
+            panel_Firmy.Size = new Size(420, 254);
+            panel_Firmy.TabIndex = 4;
+            panel_Firmy.Paint += panel_Firmy_Paint;
+            // 
+            // textBox_NIP
+            // 
+            textBox_NIP.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBox_NIP.Location = new Point(71, 164);
+            textBox_NIP.Name = "textBox_NIP";
+            textBox_NIP.Size = new Size(250, 33);
+            textBox_NIP.TabIndex = 6;
+            textBox_NIP.TextChanged += textBox_NIP_TextChanged;
+            // 
+            // textBox_NazwaFirmy
+            // 
+            textBox_NazwaFirmy.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBox_NazwaFirmy.Location = new Point(71, 45);
+            textBox_NazwaFirmy.Name = "textBox_NazwaFirmy";
+            textBox_NazwaFirmy.Size = new Size(250, 33);
+            textBox_NazwaFirmy.TabIndex = 5;
+            textBox_NazwaFirmy.TextChanged += textBox_NazwaFirmy_TextChanged;
+            // 
+            // Form_HistoriaZamowienSzczegoly
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1184, 611);
+            Controls.Add(panel_Firmy);
+            Controls.Add(dataGridView_Koszyk);
+            Controls.Add(textBox_DataZrealizowania);
+            Controls.Add(textBox_DataZlozenia);
+            Controls.Add(textBox_Klient);
+            Name = "Form_HistoriaZamowienSzczegoly";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView_Koszyk).EndInit();
+            panel_Firmy.ResumeLayout(false);
+            panel_Firmy.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox_Klient;
+        private TextBox textBox_DataZlozenia;
+        private TextBox textBox_DataZrealizowania;
+        private DataGridView dataGridView_Koszyk;
+        private Panel panel_Firmy;
+        private TextBox textBox_NIP;
+        private TextBox textBox_NazwaFirmy;
     }
 }
--- PodkladexApp/Zaopatrzenie/Form_HistoriaZamowienSzczegoly.cs
+++ PodkladexApp/Zaopatrzenie/Form_HistoriaZamowienSzczegoly.cs
@@ -10,11 +10,46 @@ using System.Windows.Forms;
 
 namespace PodkladexApp.Zaopatrzenie
 {
-    public partial class Form_HistoriaZamowienSzczegoly: Form
+    public partial class Form_HistoriaZamowienSzczegoly : Form
     {
         public Form_HistoriaZamowienSzczegoly()
         {
             InitializeComponent();
+        }
+
+        private void textBox_Klient_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_DataZlozenia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_DataZrealizowania_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_Firmy_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox_NazwaFirmy_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_NIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView_Koszyk_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
