106aa6fd71d8b62cad634a7f11962eb45f0ef52e
Szczepan-Witoslawski <szczepan.witoslawski@student.wat.edu.pl>
2026-05-12 22:05:38 +0200

    wyswietlanie efektywnosc

--- PodkladexApp/Form_Efektywnosc.cs
+++ PodkladexApp/Form_Efektywnosc.cs
@@ -14,12 +14,18 @@ namespace PodkladexApp
     {
         PodkladexContext _context;
 
+        private enum TrybWidoku { Materialy, Produkty }
+        private TrybWidoku aktualnyTryb;
+
         public Form_Efektywnosc(PodkladexContext db)
         {
             InitializeComponent();
             _context = db;
 
+            // Rejestracja zdarzeń
             this.Load += Form_Efektywnosc_Load;
+            this.btn_WidokMaterialy.Click += btn_WidokMaterialy_Click;
+            this.btn_WidokProdukty.Click += btn_WidokProdukty_Click;
             this.comboBox_FiltrMaszyna.SelectedIndexChanged += Filtry_SelectedIndexChanged;
             this.comboBox_FiltrPracownik.SelectedIndexChanged += Filtry_SelectedIndexChanged;
             this.comboBox_Sortowanie.SelectedIndexChanged += Filtry_SelectedIndexChanged;
@@ -31,7 +37,8 @@ namespace PodkladexApp
         private void Form_Efektywnosc_Load(object sender, EventArgs e)
         {
             ZaladujSlowniki();
-            WyczyscFiltry();
+            // Domyślnie ładujemy widok produkcji podkładek na start
+            UstawWidok(TrybWidoku.Produkty);
         }
 
         private void FormatujTabele()
@@ -45,13 +52,8 @@ namespace PodkladexApp
 
         private void ZaladujSlowniki()
         {
-            comboBox_FiltrMaszyna.SelectedIndexChanged -= Filtry_SelectedIndexChanged;
             comboBox_FiltrPracownik.SelectedIndexChanged -= Filtry_SelectedIndexChanged;
             comboBox_Sortowanie.SelectedIndexChanged -= Filtry_SelectedIndexChanged;
-
-            comboBox_FiltrMaszyna.DataSource = _context.Maszyna.ToList();
-            comboBox_FiltrMaszyna.DisplayMember = "Nazwa";
-            comboBox_FiltrMaszyna.ValueMember = "IdMaszyna";
 
             comboBox_FiltrPracownik.DataSource = _context.Pracownik
                 .Include(p => p.IdOsobaNavigation)
@@ -60,7 +62,7 @@ namespace PodkladexApp
             comboBox_FiltrPracownik.DisplayMember = "Nazwa";
             comboBox_FiltrPracownik.ValueMember = "IdPracownik";
 
-            var opcjeSortowania = new List<string>
+            comboBox_Sortowanie.DataSource = new List<string>
             {
                 "Pracownik A-Z",
                 "Maszyna A-Z",
@@ -68,89 +70,132 @@ namespace PodkladexApp
                 "Efektywność (od najniższej)",
                 "Największa produkcja [kg]"
             };
-            comboBox_Sortowanie.DataSource = opcjeSortowania;
 
-            comboBox_FiltrMaszyna.SelectedIndexChanged += Filtry_SelectedIndexChanged;
             comboBox_FiltrPracownik.SelectedIndexChanged += Filtry_SelectedIndexChanged;
             comboBox_Sortowanie.SelectedIndexChanged += Filtry_SelectedIndexChanged;
         }
+
+        // Metoda zarządzająca stanem formularza
+        private void UstawWidok(TrybWidoku tryb)
+        {
+            aktualnyTryb = tryb;
+
+            if (aktualnyTryb == TrybWidoku.Materialy)
+            {
+                btn_WidokMaterialy.BackColor = System.Drawing.Color.LightSkyBlue;
+                btn_WidokProdukty.BackColor = System.Drawing.SystemColors.Control;
+            }
+            else
+            {
+                btn_WidokProdukty.BackColor = System.Drawing.Color.LightSkyBlue;
+                btn_WidokMaterialy.BackColor = System.Drawing.SystemColors.Control;
+            }
+
+            OdswiezZalezneFiltryMaszyn();
+            WyczyscFiltry();
+        }
+
+        private void OdswiezZalezneFiltryMaszyn()
+        {
+            comboBox_FiltrMaszyna.SelectedIndexChanged -= Filtry_SelectedIndexChanged;
+
+            if (aktualnyTryb == TrybWidoku.Produkty)
+            {
+                comboBox_FiltrMaszyna.DataSource = _context.Maszyna.Where(m => m.Nazwa.Contains("Prasa")).ToList();
+            }
+            else
+            {
+                comboBox_FiltrMaszyna.DataSource = _context.Maszyna.Where(m => m.Nazwa.Contains("Gilotyna")).ToList();
+            }
+
+            comboBox_FiltrMaszyna.DisplayMember = "Nazwa";
+            comboBox_FiltrMaszyna.ValueMember = "IdMaszyna";
+            comboBox_FiltrMaszyna.SelectedIndex = -1;
+
+            comboBox_FiltrMaszyna.SelectedIndexChanged += Filtry_SelectedIndexChanged;
+        }
+
+        private void btn_WidokMaterialy_Click(object sender, EventArgs e) => UstawWidok(TrybWidoku.Materialy);
+        private void btn_WidokProdukty_Click(object sender, EventArgs e) => UstawWidok(TrybWidoku.Produkty);
+        private void btn_WyczyscFiltry_Click(object sender, EventArgs e) => WyczyscFiltry();
+        private void Filtry_SelectedIndexChanged(object sender, EventArgs e) => OdswiezDane();
 
         private void WyczyscFiltry()
         {
             comboBox_FiltrMaszyna.SelectedIndex = -1;
             comboBox_FiltrPracownik.SelectedIndex = -1;
-            comboBox_Sortowanie.SelectedIndex = 2;
-            OdswiezDane();
-        }
-
-        private void btn_WyczyscFiltry_Click(object sender, EventArgs e)
-        {
-            WyczyscFiltry();
-        }
-
-        private void Filtry_SelectedIndexChanged(object sender, EventArgs e)
-        {
+            comboBox_Sortowanie.SelectedIndex = 2; // Domyślnie "Efektywność (od najwyższej)"
             OdswiezDane();
         }
 
         private void OdswiezDane()
         {
-            var query = _context.Produkcja
-                .Include(p => p.IdZadaniePNavigation.IdMaszynaNavigation)
-                .Include(p => p.IdPracownikNavigation.IdOsobaNavigation)
-                .Include(p => p.IdZadaniePNavigation.KontrolaProd)
-                .Include(p => p.IdZadaniePNavigation.KontrolaMat)
-                .AsQueryable();
-
-            if (comboBox_FiltrMaszyna.SelectedValue is int idMaszyna)
-            {
-                query = query.Where(p => p.IdZadaniePNavigation.IdMaszyna == idMaszyna);
-            }
-
-            if (comboBox_FiltrPracownik.SelectedValue is int idPracownik)
-            {
-                query = query.Where(p => p.IdPracownik == idPracownik);
-            }
-
-            var suroweDane = query.Select(p => new
-            {
-                ID = p.IdProdukcja,
-                Zadanie = p.IdZadanieP,
-                Pracownik = p.IdPracownikNavigation.IdOsobaNavigation.Imie + " " + p.IdPracownikNavigation.IdOsobaNavigation.Nazwisko,
-                Maszyna = p.IdZadaniePNavigation.IdMaszynaNavigation.Nazwa,
-                Wyprodukowano_kg = (double)p.Wyprodukowano,
-                Odpady_kg = (double)(
-                    p.IdZadaniePNavigation.KontrolaProd.Sum(k => k.Odpady ?? 0m) +
-                    p.IdZadaniePNavigation.KontrolaMat.Sum(k => k.Odpady ?? 0m)
-                )
-            }).ToList();
-
-            var daneZObliczeniami = suroweDane.Select(x => new
-            {
-                x.ID,
-                x.Zadanie,
-                x.Pracownik,
-                x.Maszyna,
-                x.Wyprodukowano_kg,
-                x.Odpady_kg,
-                Efektywnosc = (x.Wyprodukowano_kg + x.Odpady_kg) > 0
-                    ? Math.Round((x.Wyprodukowano_kg / (x.Wyprodukowano_kg + x.Odpady_kg)) * 100, 2)
-                    : 0
-            }).ToList();
-
+            int? idMaszyna = comboBox_FiltrMaszyna.SelectedValue as int?;
+            int? idPracownik = comboBox_FiltrPracownik.SelectedValue as int?;
             string wybraneSortowanie = comboBox_Sortowanie.SelectedItem?.ToString();
-            List<dynamic> daneLista;
 
-            switch (wybraneSortowanie)
+            IEnumerable<dynamic> suroweDane;
+
+            if (aktualnyTryb == TrybWidoku.Produkty)
             {
-                case "Pracownik A-Z": daneLista = daneZObliczeniami.OrderBy(x => x.Pracownik).Cast<dynamic>().ToList(); break;
-                case "Maszyna A-Z": daneLista = daneZObliczeniami.OrderBy(x => x.Maszyna).Cast<dynamic>().ToList(); break;
-                case "Efektywność (od najwyższej)": daneLista = daneZObliczeniami.OrderByDescending(x => x.Efektywnosc).Cast<dynamic>().ToList(); break;
-                case "Efektywność (od najniższej)": daneLista = daneZObliczeniami.OrderBy(x => x.Efektywnosc).Cast<dynamic>().ToList(); break;
-                case "Największa produkcja [kg]": daneLista = daneZObliczeniami.OrderByDescending(x => x.Wyprodukowano_kg).Cast<dynamic>().ToList(); break;
-                default: daneLista = daneZObliczeniami.OrderByDescending(x => x.ID).Cast<dynamic>().ToList(); break;
+                var queryProd = _context.KontrolaProd
+                    .Include(k => k.IdZadaniePNavigation.IdMaszynaNavigation)
+                    .Include(k => k.IdZadaniePNavigation.Produkcja).ThenInclude(p => p.IdPracownikNavigation.IdOsobaNavigation)
+                    .Where(k => k.Zatwierdzone && k.IdZadaniePNavigation.IdMaszynaNavigation.Nazwa.Contains("Prasa"))
+                    .AsQueryable();
+
+                if (idMaszyna.HasValue) queryProd = queryProd.Where(k => k.IdZadaniePNavigation.IdMaszyna == idMaszyna.Value);
+                if (idPracownik.HasValue) queryProd = queryProd.Where(k => k.IdZadaniePNavigation.Produkcja.Any(p => p.IdPracownik == idPracownik.Value));
+
+                suroweDane = queryProd.ToList().Select(k => {
+                    var produkcja = k.IdZadaniePNavigation.Produkcja.FirstOrDefault();
+                    double w = produkcja != null ? (double)(produkcja.Wyprodukowano ?? 0m) : 0;
+                    double o = (double)(k.Odpady ?? 0m);
+                    string prac = produkcja != null ? $"{produkcja.IdPracownikNavigation.IdOsobaNavigation.Imie} {produkcja.IdPracownikNavigation.IdOsobaNavigation.Nazwisko}" : "Brak";
+
+                    return new
+                    {
+                        ID = k.IdKontrolaProd,
+                        Zadanie = k.IdZadanieP,
+                        Pracownik = prac,
+                        Maszyna = k.IdZadaniePNavigation.IdMaszynaNavigation.Nazwa,
+                        Wyprodukowano_kg = w,
+                        Odpady_kg = o,
+                        Efektywnosc = (w + o) > 0 ? Math.Round((w / (w + o)) * 100, 2) : 0
+                    };
+                });
+            }
+            else
+            {
+                var queryMat = _context.KontrolaMat
+                    .Include(k => k.IdZadaniePNavigation.IdMaszynaNavigation)
+                    .Include(k => k.IdZadaniePNavigation.Produkcja).ThenInclude(p => p.IdPracownikNavigation.IdOsobaNavigation)
+                    .Where(k => k.Zatwierdzone && k.IdZadaniePNavigation.IdMaszynaNavigation.Nazwa.Contains("Gilotyna"))
+                    .AsQueryable();
+
+                if (idMaszyna.HasValue) queryMat = queryMat.Where(k => k.IdZadaniePNavigation.IdMaszyna == idMaszyna.Value);
+                if (idPracownik.HasValue) queryMat = queryMat.Where(k => k.IdZadaniePNavigation.Produkcja.Any(p => p.IdPracownik == idPracownik.Value));
+
+                suroweDane = queryMat.ToList().Select(k => {
+                    var produkcja = k.IdZadaniePNavigation.Produkcja.FirstOrDefault();
+                    double w = produkcja != null ? (double)(produkcja.Wyprodukowano ?? 0m) : 0;
+                    double o = (double)(k.Odpady ?? 0m);
+                    string prac = produkcja != null ? $"{produkcja.IdPracownikNavigation.IdOsobaNavigation.Imie} {produkcja.IdPracownikNavigation.IdOsobaNavigation.Nazwisko}" : "Brak";
+
+                    return new
+                    {
+                        ID = k.IdKontrolaMat,
+                        Zadanie = k.IdZadanieP,
+                        Pracownik = prac,
+                        Maszyna = k.IdZadaniePNavigation.IdMaszynaNavigation.Nazwa,
+                        Wyprodukowano_kg = w,
+                        Odpady_kg = o,
+                        Efektywnosc = (w + o) > 0 ? Math.Round((w / (w + o)) * 100, 2) : 0
+                    };
+                });
             }
 
+            var daneLista = SortujDaneEfektywnosci(suroweDane, wybraneSortowanie);
             DGV_Efektywnosc.DataSource = daneLista;
 
             if (DGV_Efektywnosc.Columns.Count > 0)
@@ -163,6 +208,19 @@ namespace PodkladexApp
             }
 
             AktualizujWykres(daneLista);
+        }
+
+        private List<dynamic> SortujDaneEfektywnosci(IEnumerable<dynamic> dane, string sortowanie)
+        {
+            switch (sortowanie)
+            {
+                case "Pracownik A-Z": return dane.OrderBy(x => x.Pracownik).Cast<dynamic>().ToList();
+                case "Maszyna A-Z": return dane.OrderBy(x => x.Maszyna).Cast<dynamic>().ToList();
+                case "Efektywność (od najwyższej)": return dane.OrderByDescending(x => x.Efektywnosc).Cast<dynamic>().ToList();
+                case "Efektywność (od najniższej)": return dane.OrderBy(x => x.Efektywnosc).Cast<dynamic>().ToList();
+                case "Największa produkcja [kg]": return dane.OrderByDescending(x => x.Wyprodukowano_kg).Cast<dynamic>().ToList();
+                default: return dane.OrderByDescending(x => x.Efektywnosc).Cast<dynamic>().ToList();
+            }
         }
 
         private void AktualizujWykres(List<dynamic> dane)
@@ -189,7 +247,6 @@ namespace PodkladexApp
             }
             bars.ValueLabelStyle.Bold = true;
 
-            // Skonfigurowanie etykiet osi X
             ScottPlot.Tick[] xTicks = new ScottPlot.Tick[etykiety.Length];
             for (int i = 0; i < etykiety.Length; i++)
             {
@@ -200,7 +257,6 @@ namespace PodkladexApp
             formsPlot_Efektywnosc.Plot.Axes.Bottom.TickLabelStyle.Rotation = 45;
             formsPlot_Efektywnosc.Plot.Axes.Bottom.MinimumSize = 80;
 
-            // Ręczne ustawienie podziałki osi Y (tylko do 100)
             ScottPlot.Tick[] yTicks = new ScottPlot.Tick[]
             {
                 new ScottPlot.Tick(0, "0"),
@@ -213,12 +269,10 @@ namespace PodkladexApp
             formsPlot_Efektywnosc.Plot.Axes.Left.TickGenerator = new ScottPlot.TickGenerators.NumericManual(yTicks);
 
             formsPlot_Efektywnosc.Plot.Axes.SetLimitsX(-1, daneWykres.Count);
-
-            // Limit fizyczny dla tekstu, ale etykiety widoczne na osi kończą się równo na 100
             formsPlot_Efektywnosc.Plot.Axes.SetLimitsY(0, 115);
 
             formsPlot_Efektywnosc.Plot.YLabel("Efektywność [%]");
-            formsPlot_Efektywnosc.Plot.Title("Efektywność produkcji wg rekordów");
+            formsPlot_Efektywnosc.Plot.Title(aktualnyTryb == TrybWidoku.Produkty ? "Efektywność produkcji wg rekordów (Podkładki)" : "Efektywność produkcji wg rekordów (Półfabrykaty)");
 
             formsPlot_Efektywnosc.Refresh();
         }
